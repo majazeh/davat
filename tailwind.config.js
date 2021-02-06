4cbeb88497c3208eddb77a738af060af95ddee54
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-06 12:26:27 +0330

    Extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     content: ['public/*.html']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#007ba4',
+        orange: colors.orange,
+        lime: colors.lime,
+        lightBlue: colors.lightBlue
+      }
+    },
   },
   variants: {
     extend: {},
