85b19988ec494a9fd1f13ae28b2d5dbab4408a6b
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-06 14:01:44 +0330

    Create brand shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: '#007ba4',
+        brand: {
+          DEFAULT: '#007BA4',
+          '50': '#8BE2FF',
+          '100': '#71DCFF',
+          '200': '#3ECFFF',
+          '300': '#0BC2FF',
+          '400': '#00A1D7',
+          '500': '#007BA4',
+          '600': '#005571',
+          '700': '#002F3E',
+          '800': '#00080B',
+          '900': '#000000'
+        },
         orange: colors.orange,
         lime: colors.lime,
         lightBlue: colors.lightBlue
